5ed8a4c25e894af291d56405cc8fc50ad21d50d8
David <david@ologybrewing.com>
2026-04-10 22:40:09 -0400

    Add request debug logging to function handler

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,6 +4,12 @@ const app = require('../../server/index');
 const handler = serverless(app);
 
 exports.handler = async (event, context) => {
+  console.log('Event method:', event.httpMethod);
+  console.log('Event path:', event.path);
+  console.log('Event body:', event.body);
+  console.log('Event isBase64Encoded:', event.isBase64Encoded);
+  console.log('Event headers content-type:', event.headers && event.headers['content-type']);
+
   // Ensure path starts with /api for Express routing
   const p = event.path || '/';
   event.path = p.startsWith('/api') ? p : '/api' + p;
